f957c10750953e0c54289e975d42e31c425ea519
BenBoonya <benbenn223@gmail.com>
2018-10-18 00:46:41 +0700

    convert to Kotlin

---
+++ app/src/main/java/com/skooldio/android/myfirstapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.skooldio.android.myfirstapp
+
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resetButton: Button
+    private lateinit var countButton: Button
+    private lateinit var randomButton: Button
+    private lateinit var numberTextView: TextView
+    private lateinit var layout: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        layout = findViewById(R.id.constraintLayout)
+        resetButton = findViewById(R.id.resetButton)
+        countButton = findViewById(R.id.countButton)
+        randomButton = findViewById(R.id.randomButton)
+        numberTextView = findViewById(R.id.numberTextView)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        numberTextView.text = "0"
+
+        countButton.setOnClickListener { countMe(numberTextView) }
+
+        resetButton.setOnClickListener {
+            numberTextView.text = "0"
+            updateBackgroundColor(0)
+        }
+
+        randomButton.setOnClickListener {
+            val randomInt = randomInt(50)
+            numberTextView.text = randomInt.toString()
+            updateBackgroundColor(randomInt)
+        }
+    }
+
+    private fun randomInt(maximum: Int): Int {
+        val random = Random()
+        return random.nextInt(maximum) + 1
+    }
+
+    private fun countMe(textView: TextView) {
+        val countString = textView.text.toString()
+        var count: Int = Integer.parseInt(countString)
+        count++
+        updateBackgroundColor(count)
+        textView.text = count.toString()
+    }
+
+    private fun updateBackgroundColor(number: Int) {
+
+        val colorRes: Int?
+        if (number in 1..10) {
+            colorRes = ContextCompat.getColor(this, android.R.color.holo_red_dark)
+        } else if (number > 10 && number <= 20) {
+            colorRes = ContextCompat.getColor(this, android.R.color.holo_green_dark)
+        } else if (number > 20 && number <= 30) {
+            colorRes = ContextCompat.getColor(this, android.R.color.holo_blue_dark)
+        } else if (number > 30 && number <= 40) {
+            colorRes = ContextCompat.getColor(this, android.R.color.holo_orange_dark)
+        } else if (number > 40 && number <= 50) {
+            colorRes = ContextCompat.getColor(this, android.R.color.holo_purple)
+        } else {
+            colorRes = ContextCompat.getColor(this, android.R.color.black)
+        }
+        layout.setBackgroundColor(colorRes)
+    }
+}
