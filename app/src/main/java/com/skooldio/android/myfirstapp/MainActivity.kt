08b9dd883ef2264525d2950357452b608bf8d243
BenBoonya <benbenn223@gmail.com>
2018-10-23 12:58:58 +0700

    delegating variable declaration

--- app/src/main/java/com/skooldio/android/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/myfirstapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.skooldio.android.myfirstapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -12,10 +14,22 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val FIRST_RANGE_COUNTER = "first_range_counter"
+        const val SECOND_RANGE_COUNTER = "second_range_counter"
+        const val THIRD_RANGE_COUNTER = "third_range_counter"
+        const val FOURTH_RANGE_COUNTER = "fourth_range_counter"
+        const val FIFTH_RANGE_COUNTER = "fifth_range_counter"
+        const val OTHER_COUNTER = "other_counter"
+    }
+
+    private lateinit var sharePreference: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sharePreference = PreferenceManager.getDefaultSharedPreferences(this)
         initUi()
     }
 
@@ -64,16 +78,40 @@ class MainActivity : AppCompatActivity() {
     private fun updateBackgroundColor(number: Int) {
         //the component name should be exactly the same as id in XML
         constraintLayout.setBackgroundColor(when (number) {
-            in 1..10 -> ContextCompat.getColor(this, android.R.color.holo_red_dark)
-            in 11..20 -> ContextCompat.getColor(this, android.R.color.holo_green_dark)
-            in 21..30 -> ContextCompat.getColor(this, android.R.color.holo_blue_dark)
-            in 31..40 -> ContextCompat.getColor(this, android.R.color.holo_orange_dark)
-            in 41..50 -> ContextCompat.getColor(this, android.R.color.holo_purple)
-            else -> ContextCompat.getColor(this, android.R.color.black)
+            in 1..10 -> {
+                increaseCounter(FIRST_RANGE_COUNTER)
+                ContextCompat.getColor(this, android.R.color.holo_red_dark)
+            }
+            in 11..20 -> {
+                increaseCounter(SECOND_RANGE_COUNTER)
+                ContextCompat.getColor(this, android.R.color.holo_green_dark)
+            }
+            in 21..30 -> {
+                increaseCounter(THIRD_RANGE_COUNTER)
+                ContextCompat.getColor(this, android.R.color.holo_blue_dark)
+            }
+            in 31..40 -> {
+                increaseCounter(FOURTH_RANGE_COUNTER)
+                ContextCompat.getColor(this, android.R.color.holo_orange_dark)
+            }
+            in 41..50 -> {
+                increaseCounter(FIFTH_RANGE_COUNTER)
+                ContextCompat.getColor(this, android.R.color.holo_purple)
+            }
+            else -> {
+                increaseCounter(OTHER_COUNTER)
+                ContextCompat.getColor(this, android.R.color.black)
+            }
         })
     }
 
     private fun navigateToStatScreen() {
         startActivity(Intent(this, StatActivity::class.java))
+    }
+
+    private fun increaseCounter(key: String) {
+        var counter = sharePreference.getInt(key, 0)
+        counter++
+        sharePreference.edit().putInt(key, counter).apply()
     }
 }
--- app/src/main/java/com/skooldio/android/myfirstapp/StatActivity.kt
+++ app/src/main/java/com/skooldio/android/myfirstapp/StatActivity.kt
@@ -1,16 +1,25 @@
 package com.skooldio.android.myfirstapp
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_stat.*
 
 class StatActivity : AppCompatActivity() {
+
+    private val sharePreference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stat)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        initUi()
+    }
+
+    private fun initUi() {
+        firstRangeTextView.text = sharePreference.getInt(MainActivity.FIRST_RANGE_COUNTER, 0).toString()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
