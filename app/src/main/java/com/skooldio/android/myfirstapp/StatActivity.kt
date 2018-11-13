71130846f49ad48e0d805e08d20ee8c7ab593e61
BenBoonya <benbenn223@gmail.com>
2018-11-13 20:58:39 +0700

    init sharepreference along with example

--- app/src/main/java/com/skooldio/android/myfirstapp/StatActivity.kt
+++ app/src/main/java/com/skooldio/android/myfirstapp/StatActivity.kt
@@ -1,16 +1,21 @@
 package com.skooldio.android.myfirstapp
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_stat.*
 
 class StatActivity : AppCompatActivity() {
+
+    private val sharePreference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stat)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        firstRangeTextView.text = sharePreference.getInt(MainActivity.FIRST_RANGE_COUNTER, 0).toString()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
